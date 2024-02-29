8f0fff146bb695e29aacbe83c278037721f260aa
nebrosarth <etozhemadd@bk.ru>
2024-02-29 19:09:36 +0500

    add html result page

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from transformers import pipeline
 from pydantic import BaseModel
-
+from jinja2 import Environment, FileSystemLoader
 
 class Item(BaseModel):
     text: str
 
-
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+template = env.get_template('result.html')
 
 @app.get("/")
 def root():
     return {"FastApi service started!"}
 
+@app.get("/{text}", response_class=HTMLResponse)
+def get_params(request: Request, text: str):
+    prediction_result = classifier(text)
+    rendered_template = template.render(result=prediction_result)
+    return HTMLResponse(content=rendered_template)
 
-@app.get("/{text}")
-def get_params(text: str):
-    return classifier(text)
 
 
 @app.post("/predict/")
