f6edd1dd3b436cded8b4621d49e6187554f35186
RoGr <96972624+Your-RoGr@users.noreply.github.com>
2024-06-02 14:20:22 +0500

    Создание документации (на приложение, на методы API и т.п.).

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
-
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
@@ -22,3 +22,11 @@ def get_params(text: str) -> dict:
 @app.post("/predict/")
 def predict(item: Item) -> dict:
     return classifier(item.text)
+
+# Create documentation for the API endpoints
+app.openapi_schema = get_openapi(
+    title="Sentiment Analysis API",
+    version="1.0.0",
+    description="API for sentiment analysis",
+    routes=app.routes,
+)
