d42480e85de2da96744aa07bd908dd634c9eb117
RoGr <96972624+Your-RoGr@users.noreply.github.com>
2024-06-06 13:10:12 +0500

    Изменен стиль кода, переработаны все endpoint, добавлен endpoint /favicon.ico

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
-from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+import os
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 classifier = pipeline("sentiment-analysis")
 
-
-class Item(BaseModel):
+class Request(BaseModel):
     text: str
 
-@app.get("/")
-def root() -> dict:
-    return {"message": "FastApi service started!"}
+class RequestItems(BaseModel):
+    items: list[str]
 
-@app.get("/{text}")
-def get_params(text: str) -> dict:
-    return classifier(text)
+class Response(BaseModel):
+    response: str
 
-@app.post("/predict/")
-def predict(item: Item) -> dict:
-    return classifier(item.text)
+@app.get("/", response_model=Response)
+async def root():
+    try:
+        return JSONResponse(content="FastApi service started!", media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error " + str(e))
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = "favicon.ico"
+    file_path = os.path.join("static", file_name)
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
+
+@app.post("/predict/", response_model=Response)
+async def predict(request: Request):
+    try:
+        return JSONResponse(content=classifier(request.text), media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error " + str(e))
+
+@app.post("/batch_predict/", response_model=Response)
+async def batch_predict(requestItems: RequestItems):
+    try:
+        results = []
+        for item in requestItems.items:
+            results.append(classifier(item))
+        return JSONResponse(content=results, media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error " + str(e))
 
 # Create documentation for the API endpoints
 app.openapi_schema = get_openapi(
@@ -30,3 +58,7 @@ app.openapi_schema = get_openapi(
     description="API for sentiment analysis",
     routes=app.routes,
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
